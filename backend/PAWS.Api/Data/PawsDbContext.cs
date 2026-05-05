f7e6419165e0df1fa0fa14b2f724b09ff7829f9d
drjstancer <drj.stancer@gmail.com>
2026-05-04 23:22:31 -0500

    Add EF Core DbContext

---
+++ backend/PAWS.Api/Data/PawsDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using PAWS.Api.Models;
+
+namespace PAWS.Api.Data
+{
+    public class PawsDbContext : DbContext
+    {
+        public PawsDbContext(DbContextOptions<PawsDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<Requirement> Requirements => Set<Requirement>();
+        public DbSet<StudentRequirementStatus> StudentRequirementStatuses => Set<StudentRequirementStatus>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasIndex(s => s.MuId).IsUnique();
+        }
+    }
+}

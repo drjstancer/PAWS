470eb3387a2439cbbebee6b7bb573c207df57680
drjstancer <drj.stancer@gmail.com>
2026-05-04 23:34:39 -0500

    Add applicability DbSet

--- backend/PAWS.Api/Data/PawsDbContext.cs
+++ backend/PAWS.Api/Data/PawsDbContext.cs
@@ -10,6 +10,7 @@ namespace PAWS.Api.Data
         public DbSet<Student> Students => Set<Student>();
         public DbSet<Requirement> Requirements => Set<Requirement>();
         public DbSet<StudentRequirementStatus> StudentRequirementStatuses => Set<StudentRequirementStatus>();
+        public DbSet<RequirementApplicability> RequirementApplicabilities => Set<RequirementApplicability>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

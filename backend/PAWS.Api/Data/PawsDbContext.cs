ae44a54d9fe2a977353eb43816864fb4757a551d
drjstancer <drj.stancer@gmail.com>
2026-05-05 00:04:21 -0500

    Register operational entities in DbContext

--- backend/PAWS.Api/Data/PawsDbContext.cs
+++ backend/PAWS.Api/Data/PawsDbContext.cs
@@ -12,9 +12,29 @@ namespace PAWS.Api.Data
         public DbSet<StudentRequirementStatus> StudentRequirementStatuses => Set<StudentRequirementStatus>();
         public DbSet<RequirementApplicability> RequirementApplicabilities => Set<RequirementApplicability>();
 
+        public DbSet<TrackStatus> TrackStatuses => Set<TrackStatus>();
+        public DbSet<StudentStatus> StudentStatuses => Set<StudentStatus>();
+        public DbSet<Advisor> Advisors => Set<Advisor>();
+        public DbSet<AdvisorRole> AdvisorRoles => Set<AdvisorRole>();
+        public DbSet<AdvisorRoleAssignment> AdvisorRoleAssignments => Set<AdvisorRoleAssignment>();
+        public DbSet<StudentAdvisorAssignment> StudentAdvisorAssignments => Set<StudentAdvisorAssignment>();
+        public DbSet<RucaCategory> RucaCategories => Set<RucaCategory>();
+        public DbSet<RucaCode> RucaCodes => Set<RucaCode>();
+
+        public DbSet<AcademicRecord> AcademicRecords => Set<AcademicRecord>();
+        public DbSet<CourseRecord> CourseRecords => Set<CourseRecord>();
+        public DbSet<ShadowingWorkflow> ShadowingWorkflows => Set<ShadowingWorkflow>();
+        public DbSet<AdvisingMeeting> AdvisingMeetings => Set<AdvisingMeeting>();
+        public DbSet<Event> Events => Set<Event>();
+        public DbSet<StudentEventParticipation> StudentEventParticipations => Set<StudentEventParticipation>();
+        public DbSet<AlumniOutcome> AlumniOutcomes => Set<AlumniOutcome>();
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Student>().HasIndex(s => s.MuId).IsUnique();
+            modelBuilder.Entity<StudentRequirementStatus>().HasIndex(s => new { s.StudentId, s.RequirementId, s.RequirementCycle }).IsUnique();
+            modelBuilder.Entity<ShadowingWorkflow>().HasIndex(s => new { s.StudentId, s.ShadowingCycle }).IsUnique();
+            modelBuilder.Entity<StudentEventParticipation>().HasIndex(s => new { s.StudentId, s.EventId }).IsUnique();
         }
     }
 }
